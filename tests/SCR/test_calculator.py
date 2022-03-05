931b7de33d2a57820098065df9ce0e1263f5be11
Jacob8707 <shisailinghang@gmail.com>
2022-03-05 01:15:58 -0500

    branchB updated

--- tests/SCR/test_calculator.py
+++ tests/SCR/test_calculator.py
@@ -10,6 +10,16 @@ def test_substract():
 
 def test_divide():
     assert cal.divide(10,0)=='Error! The dividing number can NOT be zero'
+    assert cal.divide(10,2)==5
 
+def test_fdivide():
+    assert cal.fdivide(10,6)==1
+    assert cal.fdivide(10,0)=='Error! The dividing number can NOT be zero'
 
+def test_remainder():
+    assert cal.remainder(3,2)==1
+    assert cal.remainder(3,0)=='Error! The dividing number can NOT be zero'
+
+def test_multiply():
+    assert cal.multiply(3,6)==18
 
